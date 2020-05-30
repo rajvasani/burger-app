ebc0517dbf5374e9619e57ddcd04ada020eaa9ff
rohan <rohan@brahmin-solutions.com>
2020-05-30 17:22:08 +0530

    Added support for React Test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,7 @@ import { connect } from "react-redux";
 import * as actions from "../../store/actions/index";
 import axios from "../../axios-orders";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   };
